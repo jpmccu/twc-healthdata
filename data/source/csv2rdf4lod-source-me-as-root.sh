5a1e9a9637fbc1bc44ec7e68a8b524d7cacbc6c2
root <root@healthdata.aquarius.tw.rpi.edu>
2012-09-12 22:46:05 +0000

    var www in source me

--- data/source/csv2rdf4lod-source-me-as-root.sh
+++ data/source/csv2rdf4lod-source-me-as-root.sh
@@ -4,4 +4,5 @@
 #3     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
 
 source /srv/twc-healthdata/data/source/csv2rdf4lod-source-me-for-healthdata.sh
+source /srv/twc-healthdata/data/source/csv2rdf4lod-source-me-on-healthdata.sh
 source /srv/twc-healthdata/config/triple-store/virtuoso/csv2rdf4lod-source-me-for-virtuoso-credentials.sh

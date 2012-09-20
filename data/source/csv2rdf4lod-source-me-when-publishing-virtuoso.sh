91d46e2962babe76e351901230882cd4b0161525
root <root@healthdata.aquarius.tw.rpi.edu>
2012-09-20 20:47:46 +0000

    www dumps

--- data/source/csv2rdf4lod-source-me-when-publishing-virtuoso.sh
+++ data/source/csv2rdf4lod-source-me-when-publishing-virtuoso.sh
@@ -1,6 +1,7 @@
 export CSV2RDF4LOD_PUBLISH='true' # enables aggregation from automatic/* into publish/*
 
 export CSV2RDF4LOD_PUBLISH_COMPRESS='true'
+export CSV2RDF4LOD_PUBLISH_VARWWW_DUMP_FILES='true'
 
 export CSV2RDF4LOD_PUBLISH_TDB='false'
 export CSV2RDF4LOD_PUBLISH_VIRTUOSO='true'

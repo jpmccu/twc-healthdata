ed3b1016b4e08b24f05a4278db7f34f48fcfdc35
root <root@healthdata.aquarius.tw.rpi.edu>
2012-09-29 20:53:53 +0000

    removed fals full load in virtuoso load

--- data/source/csv2rdf4lod-source-me-when-publishing-virtuoso.sh
+++ data/source/csv2rdf4lod-source-me-when-publishing-virtuoso.sh
@@ -3,7 +3,6 @@ export CSV2RDF4LOD_PUBLISH='true' # enables aggregation from automatic/* into pu
 export CSV2RDF4LOD_PUBLISH_COMPRESS='true'
 export CSV2RDF4LOD_PUBLISH_VARWWW_DUMP_FILES='true'
 
-export CSV2RDF4LOD_PUBLISH_FULL_CONVERSIONS='false'
 export CSV2RDF4LOD_PUBLISH_SUBSET_SAMPLES='true'
 
 export CSV2RDF4LOD_PUBLISH_TDB='false'

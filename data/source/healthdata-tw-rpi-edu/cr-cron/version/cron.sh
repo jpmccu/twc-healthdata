bd873fbafbce18719f810b6e4230bfcf67684186
Tim L <timrdf@gmail.com>
2012-10-02 13:44:49 +0000

    cron stub

--- data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh
+++ data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+#3> <> rdfs:seeAlso <https://github.com/jimmccusker/twc-healthdata/wiki/Automation>;
+#3>    prov:specializationOf <https://raw.github.com/jimmccusker/twc-healthdata/master/data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh> .
+#
+#3> <https://raw.github.com/jimmccusker/twc-healthdata/master/data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh>
+#3>    foaf:homepage <https://github.com/jimmccusker/twc-healthdata/blob/master/data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh> .
 
 pushd `dirname $0` &> /dev/null
    source ../../../csv2rdf4lod-source-me-as-root.sh

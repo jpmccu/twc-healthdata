651d25c84ff7a653b661f7aada2a78a329c0a3fb
root <root@healthdata.aquarius.tw.rpi.edu>
2012-10-02 13:17:07 +0000

    credentials

---
+++ data/source/csv2rdf4lod-source-me-credentials.sh
@@ -0,0 +1,3 @@
+source /srv/twc-healthdata/config/triple-store/virtuoso/csv2rdf4lod-source-me-for-virtuoso-credentials.sh
+source /srv/twc-healthdata/config/google-maps/csv2rdf4lod-source-me-for-gmaps-api-key.sh
+source /srv/twc-healthdata/config/ckan/csv2rdf4lod-source-me-for-ckan-api-key.sh

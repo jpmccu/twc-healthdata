084c60426e7741bf42e39728472e06c8cb8aaf82
Tim L <timrdf@gmail.com>
2012-10-10 20:35:10 +0000

    cron

--- data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh
+++ data/source/healthdata-tw-rpi-edu/cr-cron/version/cron.sh
@@ -27,7 +27,7 @@ pushd $conversion_root &> /dev/null
    if [ `which git` ]; then
       git pull 2>&1                     >> $log
    else
-      echo "ERROR: git is not on PATH"  >> $log
+      echo "ERROR: git is not on PATH." >> $log
    fi
    echo "END cron git pull `date`"      >> $log
    echo                                 >> $log
